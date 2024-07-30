5e18e2eadb71717cd0b4fc47ef080172c877ebc4
osamads <abidusama8@gmail.com>
2024-07-30 16:26:09 +1000

    fix all register controller bug

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -20,12 +20,12 @@ const registerUser = asyncHandler( async(req, res) => {
     console.log("email:", email);
 
     if (
-        [fullName, email, username, password].some(() => field?.trim()==="")
+        [fullName, email, username, password].some((field) => field?.trim()==="")
     ) {
         throw new ApiError(400, "All fields are required")
     }
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{username}, {email}]
     })
 
@@ -34,7 +34,10 @@ const registerUser = asyncHandler( async(req, res) => {
     }
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
 
     if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar file is required")
@@ -62,7 +65,7 @@ const registerUser = asyncHandler( async(req, res) => {
         throw new ApiError(500, "Something went wrong while registering the user")
     }
 
-    return res.status(201).json(ApiResponse(200, createdUser, "user registered succesfully"))
+    return res.status(201).json(new ApiResponse(200, createdUser, "user registered succesfully"))
 
 
 } )
